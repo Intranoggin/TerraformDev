ab3ffefbc246b95bd534bf0278cab56e31ad6aa9
Ryan Miller <ryan@intranoggin.com>
2020-03-12 13:14:59 -0400

    Basic WebApp V1

--- BasicWebApp/README.md
+++ BasicWebApp/README.md
@@ -1,8 +1,7 @@
 # TerraformDev
 
 ## Setup
+
 Examples and scripts that I put together here as I go through the process of learning Terraform.
 
 The starting point for this example was my [Basic Resource Group Example](../BasicRG/) and the [Terraform App Service documentation](https://registry.terraform.io/providers/hashicorp/azurerm/2.1.0/docs/resources/app_service)
-
-
--- BasicWebApp/main.tf
+++ BasicWebApp/main.tf
@@ -5,46 +5,27 @@ provider "azurerm" {
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "BasicWebApp" {
     name     = "${var.prefix}BasicWebAppRG${var.environment}"
     location = var.location
     tags     = var.tags
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_app_service_plan" "BasicWebApp" {
+    name     = "${var.prefix}basicwebappplan${var.environment}"
+    location = azurerm_resource_group.BasicWebApp.location
+    resource_group_name = azurerm_resource_group.BasicWebApp.name
+    tags     = var.tags
 
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = "Free"
+    size = "F1"
   }
 }
 
-resource "azurerm_app_service" "example" {
-  name                = "example-app-service"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
-  }
-
-  connection_string {
-    name  = "Database"
-    type  = "SQLServer"
-    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
-  }
+resource "azurerm_app_service" "BasicWebApp" {
+  name                = "${var.prefix}basicwebappservice${var.environment}"
+  location            = azurerm_resource_group.BasicWebApp.location
+  resource_group_name = azurerm_resource_group.BasicWebApp.name
+  app_service_plan_id = azurerm_app_service_plan.BasicWebApp.id
 }
