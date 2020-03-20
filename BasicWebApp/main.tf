362c3a1f5231c51add0f1ed182f28af92aaeebe3
Ryan Miller <ryan@intranoggin.com>
2020-03-20 09:42:57 -0400

    Converted back to free tier with windows host

--- BasicWebApp/main.tf
+++ BasicWebApp/main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "BasicWebApp" {
-    name     = "${var.prefix}BasicWebAppRG2${var.environment}"
+    name     = "${var.prefix}BasicWebAppRG${var.environment}"
     location = var.location
     tags     = var.tags  
 }
@@ -15,13 +15,11 @@ resource "azurerm_app_service_plan" "BasicWebApp" {
     name     = "${var.prefix}BasicWebAppPlan${var.environment}"
     location = azurerm_resource_group.BasicWebApp.location
     resource_group_name = azurerm_resource_group.BasicWebApp.name
-    kind = "Linux"
-    reserved = true
     tags     = var.tags
 
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = "Free"
+    size = "F1"
   }
 }
 
@@ -29,9 +27,5 @@ resource "azurerm_app_service" "BasicWebApp" {
   name                = "${var.prefix}BasicWebAppService${var.environment}"
   location            = azurerm_resource_group.BasicWebApp.location
   resource_group_name = azurerm_resource_group.BasicWebApp.name
-  app_service_plan_id = azurerm_app_service_plan.BasicWebApp.id  
-
-  site_config {
-    linux_fx_version = "NODE|10.14"
-  }
+  app_service_plan_id = azurerm_app_service_plan.BasicWebApp.id 
 }
